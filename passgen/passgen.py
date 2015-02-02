9e4bd973d43cec367dbf90b80c54e27fa537ddc3
Lana Larsen <lana@stoatly.com>
2015-02-02 09:56:44 -0800

    formatting changes per PEP 8

--- passgen/passgen.py
+++ passgen/passgen.py
@@ -66,10 +66,12 @@ if __name__ == '__main__':
     parser.add_argument('--uppercase', '-u', action='store_true')
     parser.add_argument('--digits', '-d', action='store_true')
     parser.add_argument('--symbols', '-s', action='store_true')
-    parser.add_argument('--no-ambiguous', '-a', action='store_false', 
-        dest='ambiguous', default=True)
-    parser.add_argument('--no-vowels', '-v', action='store_false', 
-        dest='vowels', default=True)
+    parser.add_argument(
+        '--no-ambiguous', '-a', action='store_false', dest='ambiguous',
+         default=True)
+    parser.add_argument(
+        '--no-vowels', '-v', action='store_false', dest='vowels', 
+        default=True)
     args = parser.parse_args()
     password = passgen(**args.__dict__)
     print(password)
